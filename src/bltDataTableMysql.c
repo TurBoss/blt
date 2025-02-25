7a697766515ed5c7a190134c44bb4e8f2880fdd6
TurBoss <j.l.toledano.l@gmail.com>
2025-02-25 09:23:34 +0100

    Update bltDataTableMysql.c

--- src/bltDataTableMysql.c
+++ src/bltDataTableMysql.c
@@ -334,7 +334,7 @@ MysqlConnect(Tcl_Interp *interp, const char *host, const char *user,
     
     
     
-    bool reconnect = true;
+    int reconnect = 1;
     mysql_options(cp, MYSQL_OPT_RECONNECT, reconnect);
     
     //cp->reconnect = 1;
@@ -626,7 +626,7 @@ MysqlExportValues(Tcl_Interp *interp, MYSQL *conn, BLT_TABLE table,
             bind[count].buffer_type = MYSQL_TYPE_STRING;
             bind[count].buffer = (char *)label;
             bind[count].buffer_length = strlen(label);
-            bind[count].is_null = &false;
+            bind[count].is_null = false;
             count++;
         }
         for (col = blt_table_first_tagged_column(&argsPtr->ci); col != NULL;
@@ -635,7 +635,7 @@ MysqlExportValues(Tcl_Interp *interp, MYSQL *conn, BLT_TABLE table,
                 bind[count].buffer_type = MYSQL_TYPE_STRING;
                 bind[count].buffer = (char *)"";
                 bind[count].buffer_length = 0;
-                bind[count].is_null = &true;
+                bind[count].is_null = true;
             } else {
                 BLT_TABLE_VALUE value;
 
@@ -645,7 +645,7 @@ MysqlExportValues(Tcl_Interp *interp, MYSQL *conn, BLT_TABLE table,
                 bind[count].buffer_type = MYSQL_TYPE_STRING;
                 bind[count].buffer = (char *)blt_table_value_string(value);
                 bind[count].buffer_length = blt_table_value_length(value);
-                bind[count].is_null = &false;
+                bind[count].is_null = false;
             }
             count++;
         }
